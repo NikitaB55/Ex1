32293f125cf9fd3d7d76805dfb2baba3af52bbee
NikitaB55 <nikitabkn555@gmail.com>
2024-12-10 14:38:19 +0200

    2nd commit and push

--- src/assignments/ex1/Ex1.java
+++ src/assignments/ex1/Ex1.java
@@ -20,7 +20,7 @@ public class Ex1 {
      */
     public static int number2Int(String num) {
         int ans = -1;
-        // add your code here
+        // add your code here1
 
         ////////////////////
         return ans;
