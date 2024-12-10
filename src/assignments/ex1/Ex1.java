6304cf50024334cfe947c29e6f69b005a9aaf574
NikitaB55 <nikitabkn555@gmail.com>
2024-12-10 15:09:50 +0200

    work

--- src/assignments/ex1/Ex1.java
+++ src/assignments/ex1/Ex1.java
@@ -20,7 +20,7 @@ public class Ex1 {
      */
     public static int number2Int(String num) {
         int ans = -1;
-        // add your code here11
+        // add your code here1111
 
         ////////////////////
         return ans;
