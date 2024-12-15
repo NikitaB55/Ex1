035edc46e0129ce87f670ad816c60357d1f7ff8d
NikitaB55 <nikitabkn555@gmail.com>
2024-12-16 01:07:25 +0200

    final tweaks

--- src/assignments/ex1/Ex1Main.java
+++ src/assignments/ex1/Ex1Main.java
@@ -14,20 +14,31 @@ public class Ex1Main {
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
             System.out.println("Ex1 class solution:");
-            System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
+            System.out.println("Enter a string as number#1 with a base from 2-G (or \"quit\" to end the program): ");
             num1 = sc.next();
+            System.out.println();
             if (!num1.equals("quit")) {
                 System.out.printf(num1 + " is number: " + Ex1.isNumber(num1) + ", value = " + Ex1.number2Int(num1));
-                System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
+                System.out.println("Enter a string as number#2 with a base from 2-G (or \"quit\" to end the program): ");
                 num2 = sc.next();
+                System.out.println();
                 if (!num2.equals("quit")) {
                     System.out.printf(num2 + " is number: " + Ex1.isNumber(num2) + ", value = " + Ex1.number2Int(num2));
-                    System.out.println("Enter a string as base: ");
+                    System.out.println("Enter a a base (a number 2-16): ");
                     base = sc.next();
-                    if ((Integer.parseInt(base) > 1 && Integer.parseInt(base) < 17) && Integer.parseInt(base) != 10) {
+                    System.out.println();
+                    if ((Integer.parseInt(base) > 1 && Integer.parseInt(base) < 10)) {
                         String sum = (Integer.toString(Integer.parseInt(String.valueOf(Ex1.number2Int(num1) + Ex1.number2Int(num2)), 10), Integer.parseInt(base)) + "b" + base);
                         System.out.println(num1 + " + " + num2 + " = " + sum);
                         String mul = (Integer.toString(Integer.parseInt(String.valueOf(Ex1.number2Int(num1) * Ex1.number2Int(num2)), 10), Integer.parseInt(base)) + "b" + base);
+                        System.out.println(num1 + " * " + num2 + " = " + mul);
+                        String[] arr1 = {num1, num2, sum, mul};
+                        System.out.println("Max number over [" + num1 + "," + num2 + "," + sum + "," + mul + "] is: " + arr1[Ex1.maxIndex(arr1)]);
+                    }
+                    else if ((Integer.parseInt(base) > 10 && Integer.parseInt(base) < 17)) {
+                        String sum = (Integer.toString(Integer.parseInt(String.valueOf(Ex1.number2Int(num1) + Ex1.number2Int(num2)), 10), Integer.parseInt(base)) + "b" + Ex1.numberToLetter(Integer.parseInt(base)));
+                        System.out.println(num1 + " + " + num2 + " = " + sum);
+                        String mul = (Integer.toString(Integer.parseInt(String.valueOf(Ex1.number2Int(num1) * Ex1.number2Int(num2)), 10), Integer.parseInt(base)) + "b" + Ex1.numberToLetter(Integer.parseInt(base)));
                         System.out.println(num1 + " * " + num2 + " = " + mul);
                         String[] arr1 = {num1, num2, sum, mul};
                         System.out.println("Max number over [" + num1 + "," + num2 + "," + sum + "," + mul + "] is: " + arr1[Ex1.maxIndex(arr1)]);
